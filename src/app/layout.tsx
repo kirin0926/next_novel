c9b52ebfd25a2a9f77e5e4df6e958a0d3913c33f
novelshort <yq963657293@outlook.com>
2025-01-15 11:35:07 +0800

    新增谷歌广告

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Layout } from "@/components/layout";
 import { ClerkProvider } from '@clerk/nextjs'
+import Script from 'next/script'
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,12 +29,17 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <head>
-          <script 
-            async 
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7897104007345492"
-            crossOrigin="anonymous"
-          />
-          <script defer data-domain="nicenovel.org" src="https://plausible.io/js/script.outbound-links.pageview-props.revenue.tagged-events.js"></script>
+        {process.env.NODE_ENV === 'production' && (
+          <>
+            <script
+              async
+              src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7897104007345492"
+              crossOrigin="anonymous"
+            />
+            <script defer data-domain="nicenovel.org" src="https://plausible.io/js/script.outbound-links.pageview-props.revenue.tagged-events.js"></script>
+          </>
+        )}
+          
         </head>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <Layout>{children}</Layout>
